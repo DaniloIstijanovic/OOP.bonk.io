95c26802d472c7cc971e40fa11dcd997349979fa
Cabbage-Roll <62716710+Cabbage-Roll@users.noreply.github.com>
2021-03-04 22:45:33 +0100

    Create Property.java

---
+++ src/oop/bonk/io/Property.java
@@ -0,0 +1,130 @@
+package oop.bonk.io;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Property {
+    private double base;
+    // ako je negativno nez nam sta onda recimo da je korisnik glup za sad
+    private int delay;
+    // povecanje ili smanjenje po sekundi
+    private double delta;
+    private double limit;
+
+    // false ako je delta < 0, true ako je delta > 0, ubrzava kod
+    private boolean mode;
+    // promenljiva koju zapravo aktivno koristimo
+    private double workingValue;
+
+    private Thread thread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            while (mode ? (getWorkingValue() < getLimit()) : (getLimit() < getWorkingValue())) {
+                tick();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            workingValue = limit;
+        }
+    });
+
+    public double getWorkingValue() {
+        return workingValue;
+    }
+
+    private int getIncreaseDelay() {
+        return delay;
+    }
+
+    private double getLimit() {
+        return limit;
+    }
+
+    private void tick() {
+        workingValue += delta;
+    }
+
+    public void start() {
+        workingValue = base;
+        if (delta == 0) {
+            Main.debug(Main.DebugReason.WARNING, "delta je 0, treba nesto uraditi");
+        } else {
+            Timer timer = new Timer();
+
+            TimerTask task = new TimerTask() {
+                @Override
+                public void run() {
+                    thread.start();
+                }
+            };
+
+            timer.schedule(task, getIncreaseDelay() * 1000);
+        }
+    }
+
+    public void pause() {
+        thread.interrupt();
+    }
+
+    public void unPause() {
+        thread.start();
+    }
+
+    public static class Builder {
+
+        private double base;
+        private int delay;
+        private double delta;
+        private double limit;
+
+        Builder(double base) {
+            this.base = base;
+        }
+
+        public Builder base(double base) {
+            this.base = base;
+            return this;
+        }
+
+        public Builder delay(int delay) {
+            this.delay = delay;
+            return this;
+        }
+
+        public Builder delta(double increase) {
+            this.delta = increase;
+            return this;
+        }
+
+        public Builder limit(double limit) {
+            this.limit = limit;
+            return this;
+        }
+
+        public Property build() {
+            return new Property(this);
+        }
+    }
+
+    private Property(Builder builder) {
+        this.base = builder.base;
+        this.delay = builder.delay;
+        this.delta = builder.delta;
+
+        if (delta > 0) {
+            mode = true;
+        } else {
+            mode = false;
+        }
+        this.limit = builder.limit;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        Main.debug(Main.DebugReason.MEMORY, "Instanca klase " + getClass().getSimpleName() + " se finalizuje");
+    }
+}
