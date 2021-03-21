5962afc6592c898fa5b4c468a1c0dc6e421f3860
Cabbage-Roll <62716710+Cabbage-Roll@users.noreply.github.com>
2021-03-21 20:24:46 +0100

    Create FirstRun.java

---
+++ src/oop/bonk/io/FirstRun.java
@@ -0,0 +1,10 @@
+package oop.bonk.io;
+
+/*
+ * namena: koristi se kada je program ukljucen prvi put
+ * ili kad je citanje iz fajla iskljuceno
+ * ili kada nevalja fajl (onda ga namerno pokrece kao prvi put)
+ */
+public class FirstRun implements ActionListener {
+
+}
