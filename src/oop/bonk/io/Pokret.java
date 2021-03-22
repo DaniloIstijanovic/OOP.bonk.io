d38e79a41e9bd2a021f4f23aefc1ab320fbb2e68
IlijaBruh <62703289+IlijaBruh@users.noreply.github.com>
2021-03-22 10:36:20 +0100

    Add files via upload
    Pkretanje(osnovni pokreti) i gravitacija

---
+++ src/oop/bonk/io/Pokret.java
@@ -0,0 +1,96 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Pokret extends JPanel implements ActionListener, KeyListener{
+
+    Timer tm = new Timer(5, this);
+    int x = 0;
+    int y = 0;
+    int velX = 0;
+    int velY = 0;
+    int gravitacija = 1;
+
+    public Pokret(){
+        tm.start();
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.setColor(Color.RED);
+        g.fillRect(x, y, 50, 30);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if(x < 0){
+            velX = 0;
+            x = 0;
+        }
+        if(x > 600){
+            velX = 0;
+            x = 500;
+        }
+        if(y < 0){
+            velY = 0;
+            y = 0;
+        }
+        if(y > 400){
+            velY = 0;
+            y = 300;
+        }
+
+        x = x + velX;
+        y = y + velY + gravitacija;
+        repaint();
+    }
+
+    public void keyPressed(KeyEvent e){
+        int c = e.getKeyCode();
+
+        if(c == KeyEvent.VK_LEFT){
+            velX = -2;
+            velY = 0;
+        }
+        if(c == KeyEvent.VK_RIGHT){
+            velX = 2;
+            velY = 0;
+        }
+        if(c == KeyEvent.VK_UP){
+            velX = 0;
+            velY = -2;
+        }
+        if(c == KeyEvent.VK_DOWN){
+            velX = 0;
+            velY = 2;
+        }
+    
+    }
+
+    public void keyTyped(KeyEvent e){}
+    public void keyReleased(KeyEvent e){
+        velX = 0;
+        velY = 0;
+    }
+
+
+    public static void main(String[] args){
+        Pokret p = new Pokret();
+        JFrame jf = new JFrame();
+        jf.setTitle("Pokret");
+        jf.setSize(600, 400);
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.add(p);
+    }
+
+}
