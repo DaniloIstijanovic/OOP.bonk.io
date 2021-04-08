a543dfc562ce76bb2dacbce726c6a1645137281e
IlijaBruh <62703289+IlijaBruh@users.noreply.github.com>
2021-04-08 18:54:04 +0200

    Mapa
    Napravljeni zidovi platforme mape.

--- src/oop/bonk/io/Pokret.java
+++ src/oop/bonk/io/Pokret.java
@@ -6,19 +6,27 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.*;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+import javax.swing.text.FlowView;
+
 
 public class Pokret extends JPanel implements ActionListener, KeyListener{
 
     Timer tm = new Timer(5, this);
     int x = 0;
     int y = 0;
+    int x2 = 0;
+    int y2 = 0;
     int velX = 0;
     int velY = 0;
     int gravitacija = 1;
+
+    
 
     public Pokret(){
         tm.start();
@@ -38,17 +46,57 @@ public class Pokret extends JPanel implements ActionListener, KeyListener{
             velX = 0;
             x = 0;
         }
-        if(x > 600){
-            velX = 0;
-            x = 500;
-        }
         if(y < 0){
             velY = 0;
             y = 0;
         }
-        if(y > 400){
+
+        if(x > 930){
+            velX = 0;
+            x = 930;
+        }
+        if(y > 792){
             velY = 0;
-            y = 300;
+            y = 791;
+        }
+        
+        if(x < 212 && y > 559){
+            if( y < 610){
+            velY = 0;
+            y = 558;
+            }
+        }
+        if(x < 212 && y < 650){
+            if( y > 559){
+            velY = 0;
+            y = 650;
+            }
+        }
+
+        if(x > 280 && x < 610 && y > 380){
+            if( y < 440){
+            velY = 0;
+            y = 379;
+            }
+        }
+        if(x > 280 && x < 610 && y < 450){
+            if( y > 380){
+            velY = 0;
+            y = 450;
+            }
+        }
+
+        if(x > 655 && y > 545){
+            if( y < 560){
+            velY = 0;
+            y = 544;
+            }
+        }
+        if(x > 655 && y < 600){
+            if( y > 545){
+            velY = 0;
+            y = 600;
+            }
         }
 
         x = x + velX;
@@ -60,20 +108,20 @@ public class Pokret extends JPanel implements ActionListener, KeyListener{
         int c = e.getKeyCode();
 
         if(c == KeyEvent.VK_LEFT){
-            velX = -2;
+            velX = -5;
             velY = 0;
         }
         if(c == KeyEvent.VK_RIGHT){
-            velX = 2;
+            velX = 5;
             velY = 0;
         }
         if(c == KeyEvent.VK_UP){
             velX = 0;
-            velY = -2;
+            velY = -5;
         }
         if(c == KeyEvent.VK_DOWN){
             velX = 0;
-            velY = 2;
+            velY = 5;
         }
     
     }
@@ -89,7 +137,7 @@ public class Pokret extends JPanel implements ActionListener, KeyListener{
         Pokret p = new Pokret();
         JFrame jf = new JFrame();
         jf.setTitle("Pokret");
-        jf.setSize(600, 400);
+        jf.setSize(1000, 1000);
         jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.add(p);
