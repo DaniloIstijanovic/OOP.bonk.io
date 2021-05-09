80b9ea02fe6e24f02ae8353e3454a0a950bc5b26
unknown <62746673+DaniloIstijanovic@users.noreply.github.com>
2021-05-09 19:19:07 +0200

    Revert "Revert "gui-ispravak""
    This reverts commit 1ddee7d305ae9611a93f6d4fa6b00c842bdc8e6f.

--- src/main/java/oop/bonk/io/Main.java
+++ src/main/java/oop/bonk/io/Main.java
@@ -32,9 +32,7 @@ public class Main {
     public static Point WINDOWSIZE = COMMONSIZES[8];
     public static Font neoSansFont;
 
-    /*
-     * this.addMouseListener(new MouseInput());  ----- Input Misa za Button
-     */
+   
 
     // ovo je u potpunosti staticka klasa
     private Main() {
